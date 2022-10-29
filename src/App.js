1afe24bcfcec36451636d53fcd606509f17fba73
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2022-10-29 12:12:00 -0500

    enter your commit message here

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+function App(){
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+
+
+
+export default App;
